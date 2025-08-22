a936a2603f626f0facdd7c659b16d6e620250bb0
sarah hesham <sarah.h.nashaat@gmail.com>
2025-08-22 19:10:15 +0300

    changes imports tany yarab nekhlas

--- app/(buyer)/marketplace/[itemName]/page.tsx
+++ app/(buyer)/marketplace/[itemName]/page.tsx
@@ -13,7 +13,7 @@ import dynamic from "next/dynamic";
 import { useUserAuth } from "@/context/AuthFormContext";
 import { useItemSocket } from "@/hooks/useItemSocket";
 // To:
-import Loader from '@/components/common/Loader'
+import { Loader } from '@/components/common'
 // Lazy load FloatingRecorderButton for voice processing
 const FloatingRecorderButton = dynamic(
   () => import('@/components/Voice Processing/FloatingRecorderButton'),
